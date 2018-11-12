e21e5271347aa801eff40681771216427a750f70
Andrew Ghorbani <andrewghorbani@Andrews-Mac-mini.local>
2018-11-12 04:27:07 -0800

    added config file for heroku

---
+++ config.py
@@ -0,0 +1,8 @@
+class SecretKey:
+    SECRET_KEY = '9\xa4)\xce\x0c\xc6\x9a\x7fi:*}\xd9\x9d\xd8\xe8\xba\x0b\xa3\xcb6X4\x8e'
+
+class MySQL_DB:
+    DB_HOST = 'myfinancedb.cioldyovwirn.us-east-2.rds.amazonaws.com'
+    DB_USER = 'abghorba'
+    DB_PASSWORD = 'What45mine6!$'
+    DB_NAME = 'abghorba'
